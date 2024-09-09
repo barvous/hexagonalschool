af483c61a01cf962aa66cf6868d9d415a3dab518
Marcos Daniel <marcostkd20147@gmail.com>
2024-09-08 21:18:46 -0300

    Criando o projeto

---
+++ src/main/java/br/com/grimoire/hexagonalschool/HexagonalschoolApplication.java
@@ -0,0 +1,13 @@
+package br.com.grimoire.hexagonalschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/grimoire/hexagonalschool/HexagonalschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grimoire.hexagonalschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
